74517a953d11ba50262ef4d739538039ee0c12f2
Victor Zaponi <vzaponi@gmail.com>
2026-09-02 23:05:37 -0300

    style: load success confirmation styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './success-feedback.css'
 import App from './App.jsx'
 import { ThemeProvider } from './components/ThemeProvider.jsx'
 import { initializeTheme } from './utils/theme.js'
