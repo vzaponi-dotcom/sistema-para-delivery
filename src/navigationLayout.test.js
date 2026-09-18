66007fcf2f30fece36800faf393f78125dd65dde
Victor Zaponi <vzaponi@gmail.com>
2026-09-18 16:13:07 -0300

    test: align moved tables ui paths

--- src/navigationLayout.test.js
+++ src/navigationLayout.test.js
@@ -198,8 +198,8 @@ test('AreaNavigation preserva labels, callbacks e aria-current de todos os desti
 
 test('Cadastros permanece sem AreaNavigation', async (t) => {
   const h = await workspaceHarness(t)
-  const [{ default: Clients }, { default: Products }, { default: Tables }] = await Promise.all([
-    h.load('/src/pages/Clients.jsx'), h.load('/src/pages/Products.jsx'), h.load('/src/pages/Tables.jsx'),
+  const [{ default: Clients }, { default: Products }, { Tables }] = await Promise.all([
+    h.load('/src/pages/Clients.jsx'), h.load('/src/pages/Products.jsx'), h.load('/src/domains/table-service/index.js'),
   ])
   const callbacks = { onAdd() {}, onEdit() {}, onDelete() {} }
   const renderers = [
