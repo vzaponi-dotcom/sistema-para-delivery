95de0e5769bb5f6e5335d04ebbe8da5c0daa010d
Victor Zaponi <vzaponi@gmail.com>
2026-09-17 22:39:43 -0300

    test: require order detail public surface

--- src/domains/orders/ordersPublicUi.test.js
+++ src/domains/orders/ordersPublicUi.test.js
@@ -1,9 +1,10 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
-import { NewOrderRoute, OrderHistory, Orders } from './index.js'
+import { NewOrderRoute, OrderDetail, OrderHistory, Orders } from './index.js'
 
 test('Orders public boundary exposes all order surfaces', () => {
   assert.equal(typeof Orders, 'function')
+  assert.equal(typeof OrderDetail, 'function')
   assert.equal(typeof OrderHistory, 'function')
   assert.equal(typeof NewOrderRoute, 'function')
 })
