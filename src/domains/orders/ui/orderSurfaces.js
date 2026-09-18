63bce9f4ad4d9255a8511676eb5733d86a0d9587
Victor Zaponi <vzaponi@gmail.com>
2026-09-17 22:40:37 -0300

    fix: route receivables order detail through orders

--- src/domains/orders/ui/orderSurfaces.js
+++ src/domains/orders/ui/orderSurfaces.js
@@ -10,6 +10,11 @@ const loadOrderHistory = () => {
   return modules['./OrderHistory.jsx']?.default
 }
 
+const loadOrderDetail = () => {
+  const modules = import.meta.glob('./components/OrderDetail.jsx', { eager: true })
+  return modules['./components/OrderDetail.jsx']?.default
+}
+
 export function Orders(props) {
   const Component = loadOrders()
   return React.createElement(Component, props)
@@ -17,5 +22,10 @@ export function Orders(props) {
 
 export function OrderHistory(props) {
   const Component = loadOrderHistory()
+  return React.createElement(Component, props)
+}
+
+export function OrderDetail(props) {
+  const Component = loadOrderDetail()
   return React.createElement(Component, props)
 }
