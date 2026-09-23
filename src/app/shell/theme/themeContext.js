061b28a7f1e7c827b4e406ace6c8c672f2127c4a
Victor Zaponi <vzaponi@gmail.com>
2026-09-23 20:22:49 -0300

    feat: expose visual theme through theme context

--- src/app/shell/theme/themeContext.js
+++ src/app/shell/theme/themeContext.js
@@ -3,6 +3,8 @@ import { createContext, useContext } from 'react'
 export const ThemeContext = createContext({
   themePreference: 'system',
   setThemePreference: () => {},
+  visualTheme: 'classic',
+  setVisualTheme: () => {},
 })
 
 export const useTheme = () => useContext(ThemeContext)
